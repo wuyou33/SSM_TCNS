5684dd276fd23c621603b3249e3e1f774d34e2c1
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-06-22 09:42:41 +1000

    > Done: First commit after reorganization.

---
+++ main.m
@@ -0,0 +1,90 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                         %
+% Title: Simulation of system (19) from [AS14].                           %
+% Author: H. Stein Shiromoto                                              %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function main
+
+clear all
+close all
+clc
+
+% Set axis of graphs to LaTeX
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+
+
+% System Parameters
+
+global a b k km alpha beta gamma delta
+
+a     = 150; % [concentration/s]
+b     = 0.2; % [1/s]
+k     = 1;   % [concentration]
+km    = 1;   % ??
+alpha = 0.2; % [1/s]
+beta  = 0.2; % [1/s]
+gamma = 0.2; % [1/s]
+delta = 15;  % ??
+
+% Input
+global u
+
+u = 0;
+
+
+
+IC = [1,10,100];
+
+time = 0:1e-1:1e2;
+[time,simout] = ode45(@sys,time,IC);
+
+
+% Time Evolution of the Solution components
+figure
+plot(time,simout(:,1),'b-','LineWidth',2);
+hold on
+plot(time,simout(:,2),'r--','LineWidth',2);
+hold on
+plot(time,simout(:,3),'k:','LineWidth',2);
+grid on
+xlabel('Time $t$','interpreter','latex','fontsize',14);
+ylabel('Variables','interpreter','latex','fontsize',14);
+title('Time Evolution of the Components of the Solution','interpreter','latex','fontsize',14);
+legend('mRNA ($x$)','enzyme ($y$)','metabolite ($z$)','Location','Best');
+set(gca, 'FontSize', 14)
+axis tight
+grid on
+
+% 3D plot of the solution
+figure
+plot3(simout(:,1),simout(:,2),simout(:,3),'b-','LineWidth',2);
+hold on
+plot3(simout(1,1),simout(1,2),simout(1,3),'bo','LineWidth',2);
+xlabel('mRNA ($x(t)$)','interpreter','latex','fontsize',14);
+ylabel('enzyme ($y(t)$)','interpreter','latex','fontsize',14);
+zlabel('metabolite ($z(t)$)','interpreter','latex','fontsize',14);
+title('3D Plot of the Solution','interpreter','latex','fontsize',14);
+set(gca, 'FontSize', 14)
+axis tight
+grid on
+end
+
+function [out]=sys(t,input)
+
+x = input(1);
+y = input(2);
+z = input(3);
+
+global a b k km alpha beta gamma delta u
+
+xdot = a/(k + z) - b*x;
+ydot = alpha*x - beta*y;
+zdot = -delta*z/(km + z) + gamma*y + u;
+
+out = [xdot,ydot,zdot]';
+
+end
