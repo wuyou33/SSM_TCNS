58af8238a9bb77dbce79f99255841656e2bf05b5
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-09-20 13:28:53 +1000

    Change to comply with matrix dimensions

--- TheoreticalExample2/WriterMI.m
+++ TheoreticalExample2/WriterMI.m
@@ -42,7 +42,7 @@ if strcmp(Option,'Diagonal') || strcmp(Option,'Neighbor')
     for AgentsCounter = 1:NumberOfAgents - 1
         fprintf(fid, 'Block%s = ',num2str(AgentsCounter));
         fprintf(fid, 'MI(%s:%s,%s:%s);\n',num2str(NumberOfStates*AgentsCounter - 1),num2str(2*NumberOfStates*AgentsCounter - 2*(AgentsCounter-1)),num2str(NumberOfStates*AgentsCounter - 1),num2str(2*NumberOfStates*AgentsCounter - 2*(AgentsCounter-1)));
-        fprintf(fid, 'Block%s(2,2) = Block%s(2,2)/2;\n',num2str(AgentsCounter),num2str(AgentsCounter));
+        fprintf(fid, 'Block%s(1:%d,1:%d) = Block%s(1:%d,1:%d)/2;\n',num2str(AgentsCounter),NumberOfStates,NumberOfStates,num2str(AgentsCounter),NumberOfStates,NumberOfStates);
         fprintf(fid, '\n');
     end
     
