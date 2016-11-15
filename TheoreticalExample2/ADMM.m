562b0d8ddb4568bc583dc577a291b31c734a5781
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-11-15 17:04:26 +1100

    Start the algorithm

--- TheoreticalExample2/ADMM.m
+++ TheoreticalExample2/ADMM.m
@@ -6,3 +6,49 @@
 %                                                                         %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+clear all;
+close all;
+clc;
+
+W = sdpvar(2,2);
+Z = rand(2,2)
+Lambda = rand(2,2);
+
+
+
+Constraints = [W>=0];
+Objective   = norm(W-Z+Lambda,'fro');
+
+optimize(Constraints,Objective);
+NextW(1,1) = value(W(1,1));
+NextW(1,2) = value(W(1,2));
+NextW(2,1) = value(W(2,1));
+NextW(2,2) = value(W(2,2));
+
+[~,NotPositiveDefinite] = chol(NextW);
+if NotPositiveDefinite ~= 0
+    display('W is not PD')
+end
+
+
+Z = sdpvar(2,2);
+Constraints = [Z>=0];
+Objective   = norm(NextW-Z+Lambda,'fro');
+optimize(Constraints,Objective);
+NextZ(1,1) = value(Z(1,1));
+NextZ(1,2) = value(Z(1,2));
+NextZ(2,1) = value(Z(2,1));
+NextZ(2,2) = value(Z(2,2));
+[~,NotPositiveDefinite] = chol(NextZ);
+if NotPositiveDefinite ~= 0
+    display('Z is not PD')
+end
+
+
+Objective   = norm(NextW-NextZ+Lambda,'fro');
+optimize([],Objective);
+NextLambda(1,1) = value(Lambda(1,1));
+NextLambda(1,2) = value(Lambda(1,2));
+NextLambda(2,1) = value(Lambda(2,1));
+NextLambda(2,2) = value(Lambda(2,2));
+
