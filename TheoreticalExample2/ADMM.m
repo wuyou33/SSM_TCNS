327be2860fbf063f86b3c052700f0b67618b9933
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-11-16 14:35:33 +1100

    add a while condition and stopping criterion

--- TheoreticalExample2/ADMM.m
+++ TheoreticalExample2/ADMM.m
@@ -10,12 +10,15 @@ clear all;
 close all;
 clc;
 
-W = sdpvar(2,2);
+
 Z = rand(2,2)
-Lambda = rand(2,2);
+W = rand(2,2)
+Lambda = 10*rand(2,2);
+n = 0;
 
+while norm(W-Z,'fro') > 1e-6 && n <= 20
 
-
+W = sdpvar(2,2);
 Constraints = [W>=0];
 Objective   = norm(W-Z+Lambda,'fro');
 
@@ -24,31 +27,36 @@ NextW(1,1) = value(W(1,1));
 NextW(1,2) = value(W(1,2));
 NextW(2,1) = value(W(2,1));
 NextW(2,2) = value(W(2,2));
-
-[~,NotPositiveDefinite] = chol(NextW);
-if NotPositiveDefinite ~= 0
-    display('W is not PD')
-end
+W = NextW;
 
 
 Z = sdpvar(2,2);
 Constraints = [Z>=0];
-Objective   = norm(NextW-Z+Lambda,'fro');
+Objective   = norm(W-Z+Lambda,'fro');
 optimize(Constraints,Objective);
 NextZ(1,1) = value(Z(1,1));
 NextZ(1,2) = value(Z(1,2));
 NextZ(2,1) = value(Z(2,1));
 NextZ(2,2) = value(Z(2,2));
-[~,NotPositiveDefinite] = chol(NextZ);
-if NotPositiveDefinite ~= 0
+Z = NextZ;
+
+Lambda = W - Z + Lambda;
+
+n = n+1;
+
+end
+
+[~,NotPositiveDefiniteW] = chol(W);
+[~,NotPositiveDefiniteZ] = chol(Z);
+if NotPositiveDefiniteW ~= 0
+    display('W is not PD')
+    return;
+elseif NotPositiveDefiniteZ ~= 0
     display('Z is not PD')
+    return;
 end
 
 
-Objective   = norm(NextW-NextZ+Lambda,'fro');
-optimize([],Objective);
-NextLambda(1,1) = value(Lambda(1,1));
-NextLambda(1,2) = value(Lambda(1,2));
-NextLambda(2,1) = value(Lambda(2,1));
-NextLambda(2,2) = value(Lambda(2,2));
-
+W
+Z
+Lambda
