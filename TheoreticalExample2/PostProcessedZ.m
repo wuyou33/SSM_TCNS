64ecb559a59f9ce956a4b5e048a45c59559d4334
humberto.shiromoto <humberto.shiromoto@ieee.org>
2017-01-17 16:00:28 +1100

    [PostProcessedZ] New file added.

---
+++ TheoreticalExample2/PostProcessedZ.m
@@ -0,0 +1,264 @@
+VerifiedZ11 = clean(replace(Z(1,1), coefList, double(coefList)), 1e-09);
+Z11 = sdisplay(VerifiedZ11);
+
+clear VerifiedZ11;
+VerifiedZ12 = clean(replace(Z(1,2), coefList, double(coefList)), 1e-09);
+Z12 = sdisplay(VerifiedZ12);
+
+clear VerifiedZ12;
+VerifiedZ13 = clean(replace(Z(1,3), coefList, double(coefList)), 1e-09);
+Z13 = sdisplay(VerifiedZ13);
+
+clear VerifiedZ13;
+VerifiedZ14 = clean(replace(Z(1,4), coefList, double(coefList)), 1e-09);
+Z14 = sdisplay(VerifiedZ14);
+
+clear VerifiedZ14;
+VerifiedZ15 = clean(replace(Z(1,5), coefList, double(coefList)), 1e-09);
+Z15 = sdisplay(VerifiedZ15);
+
+clear VerifiedZ15;
+VerifiedZ16 = clean(replace(Z(1,6), coefList, double(coefList)), 1e-09);
+Z16 = sdisplay(VerifiedZ16);
+
+clear VerifiedZ16;
+VerifiedZ17 = clean(replace(Z(1,7), coefList, double(coefList)), 1e-09);
+Z17 = sdisplay(VerifiedZ17);
+
+clear VerifiedZ17;
+VerifiedZ18 = clean(replace(Z(1,8), coefList, double(coefList)), 1e-09);
+Z18 = sdisplay(VerifiedZ18);
+
+clear VerifiedZ18;
+VerifiedZ21 = clean(replace(Z(2,1), coefList, double(coefList)), 1e-09);
+Z21 = sdisplay(VerifiedZ21);
+
+clear VerifiedZ21;
+VerifiedZ22 = clean(replace(Z(2,2), coefList, double(coefList)), 1e-09);
+Z22 = sdisplay(VerifiedZ22);
+
+clear VerifiedZ22;
+VerifiedZ23 = clean(replace(Z(2,3), coefList, double(coefList)), 1e-09);
+Z23 = sdisplay(VerifiedZ23);
+
+clear VerifiedZ23;
+VerifiedZ24 = clean(replace(Z(2,4), coefList, double(coefList)), 1e-09);
+Z24 = sdisplay(VerifiedZ24);
+
+clear VerifiedZ24;
+VerifiedZ25 = clean(replace(Z(2,5), coefList, double(coefList)), 1e-09);
+Z25 = sdisplay(VerifiedZ25);
+
+clear VerifiedZ25;
+VerifiedZ26 = clean(replace(Z(2,6), coefList, double(coefList)), 1e-09);
+Z26 = sdisplay(VerifiedZ26);
+
+clear VerifiedZ26;
+VerifiedZ27 = clean(replace(Z(2,7), coefList, double(coefList)), 1e-09);
+Z27 = sdisplay(VerifiedZ27);
+
+clear VerifiedZ27;
+VerifiedZ28 = clean(replace(Z(2,8), coefList, double(coefList)), 1e-09);
+Z28 = sdisplay(VerifiedZ28);
+
+clear VerifiedZ28;
+VerifiedZ31 = clean(replace(Z(3,1), coefList, double(coefList)), 1e-09);
+Z31 = sdisplay(VerifiedZ31);
+
+clear VerifiedZ31;
+VerifiedZ32 = clean(replace(Z(3,2), coefList, double(coefList)), 1e-09);
+Z32 = sdisplay(VerifiedZ32);
+
+clear VerifiedZ32;
+VerifiedZ33 = clean(replace(Z(3,3), coefList, double(coefList)), 1e-09);
+Z33 = sdisplay(VerifiedZ33);
+
+clear VerifiedZ33;
+VerifiedZ34 = clean(replace(Z(3,4), coefList, double(coefList)), 1e-09);
+Z34 = sdisplay(VerifiedZ34);
+
+clear VerifiedZ34;
+VerifiedZ35 = clean(replace(Z(3,5), coefList, double(coefList)), 1e-09);
+Z35 = sdisplay(VerifiedZ35);
+
+clear VerifiedZ35;
+VerifiedZ36 = clean(replace(Z(3,6), coefList, double(coefList)), 1e-09);
+Z36 = sdisplay(VerifiedZ36);
+
+clear VerifiedZ36;
+VerifiedZ37 = clean(replace(Z(3,7), coefList, double(coefList)), 1e-09);
+Z37 = sdisplay(VerifiedZ37);
+
+clear VerifiedZ37;
+VerifiedZ38 = clean(replace(Z(3,8), coefList, double(coefList)), 1e-09);
+Z38 = sdisplay(VerifiedZ38);
+
+clear VerifiedZ38;
+VerifiedZ41 = clean(replace(Z(4,1), coefList, double(coefList)), 1e-09);
+Z41 = sdisplay(VerifiedZ41);
+
+clear VerifiedZ41;
+VerifiedZ42 = clean(replace(Z(4,2), coefList, double(coefList)), 1e-09);
+Z42 = sdisplay(VerifiedZ42);
+
+clear VerifiedZ42;
+VerifiedZ43 = clean(replace(Z(4,3), coefList, double(coefList)), 1e-09);
+Z43 = sdisplay(VerifiedZ43);
+
+clear VerifiedZ43;
+VerifiedZ44 = clean(replace(Z(4,4), coefList, double(coefList)), 1e-09);
+Z44 = sdisplay(VerifiedZ44);
+
+clear VerifiedZ44;
+VerifiedZ45 = clean(replace(Z(4,5), coefList, double(coefList)), 1e-09);
+Z45 = sdisplay(VerifiedZ45);
+
+clear VerifiedZ45;
+VerifiedZ46 = clean(replace(Z(4,6), coefList, double(coefList)), 1e-09);
+Z46 = sdisplay(VerifiedZ46);
+
+clear VerifiedZ46;
+VerifiedZ47 = clean(replace(Z(4,7), coefList, double(coefList)), 1e-09);
+Z47 = sdisplay(VerifiedZ47);
+
+clear VerifiedZ47;
+VerifiedZ48 = clean(replace(Z(4,8), coefList, double(coefList)), 1e-09);
+Z48 = sdisplay(VerifiedZ48);
+
+clear VerifiedZ48;
+VerifiedZ51 = clean(replace(Z(5,1), coefList, double(coefList)), 1e-09);
+Z51 = sdisplay(VerifiedZ51);
+
+clear VerifiedZ51;
+VerifiedZ52 = clean(replace(Z(5,2), coefList, double(coefList)), 1e-09);
+Z52 = sdisplay(VerifiedZ52);
+
+clear VerifiedZ52;
+VerifiedZ53 = clean(replace(Z(5,3), coefList, double(coefList)), 1e-09);
+Z53 = sdisplay(VerifiedZ53);
+
+clear VerifiedZ53;
+VerifiedZ54 = clean(replace(Z(5,4), coefList, double(coefList)), 1e-09);
+Z54 = sdisplay(VerifiedZ54);
+
+clear VerifiedZ54;
+VerifiedZ55 = clean(replace(Z(5,5), coefList, double(coefList)), 1e-09);
+Z55 = sdisplay(VerifiedZ55);
+
+clear VerifiedZ55;
+VerifiedZ56 = clean(replace(Z(5,6), coefList, double(coefList)), 1e-09);
+Z56 = sdisplay(VerifiedZ56);
+
+clear VerifiedZ56;
+VerifiedZ57 = clean(replace(Z(5,7), coefList, double(coefList)), 1e-09);
+Z57 = sdisplay(VerifiedZ57);
+
+clear VerifiedZ57;
+VerifiedZ58 = clean(replace(Z(5,8), coefList, double(coefList)), 1e-09);
+Z58 = sdisplay(VerifiedZ58);
+
+clear VerifiedZ58;
+VerifiedZ61 = clean(replace(Z(6,1), coefList, double(coefList)), 1e-09);
+Z61 = sdisplay(VerifiedZ61);
+
+clear VerifiedZ61;
+VerifiedZ62 = clean(replace(Z(6,2), coefList, double(coefList)), 1e-09);
+Z62 = sdisplay(VerifiedZ62);
+
+clear VerifiedZ62;
+VerifiedZ63 = clean(replace(Z(6,3), coefList, double(coefList)), 1e-09);
+Z63 = sdisplay(VerifiedZ63);
+
+clear VerifiedZ63;
+VerifiedZ64 = clean(replace(Z(6,4), coefList, double(coefList)), 1e-09);
+Z64 = sdisplay(VerifiedZ64);
+
+clear VerifiedZ64;
+VerifiedZ65 = clean(replace(Z(6,5), coefList, double(coefList)), 1e-09);
+Z65 = sdisplay(VerifiedZ65);
+
+clear VerifiedZ65;
+VerifiedZ66 = clean(replace(Z(6,6), coefList, double(coefList)), 1e-09);
+Z66 = sdisplay(VerifiedZ66);
+
+clear VerifiedZ66;
+VerifiedZ67 = clean(replace(Z(6,7), coefList, double(coefList)), 1e-09);
+Z67 = sdisplay(VerifiedZ67);
+
+clear VerifiedZ67;
+VerifiedZ68 = clean(replace(Z(6,8), coefList, double(coefList)), 1e-09);
+Z68 = sdisplay(VerifiedZ68);
+
+clear VerifiedZ68;
+VerifiedZ71 = clean(replace(Z(7,1), coefList, double(coefList)), 1e-09);
+Z71 = sdisplay(VerifiedZ71);
+
+clear VerifiedZ71;
+VerifiedZ72 = clean(replace(Z(7,2), coefList, double(coefList)), 1e-09);
+Z72 = sdisplay(VerifiedZ72);
+
+clear VerifiedZ72;
+VerifiedZ73 = clean(replace(Z(7,3), coefList, double(coefList)), 1e-09);
+Z73 = sdisplay(VerifiedZ73);
+
+clear VerifiedZ73;
+VerifiedZ74 = clean(replace(Z(7,4), coefList, double(coefList)), 1e-09);
+Z74 = sdisplay(VerifiedZ74);
+
+clear VerifiedZ74;
+VerifiedZ75 = clean(replace(Z(7,5), coefList, double(coefList)), 1e-09);
+Z75 = sdisplay(VerifiedZ75);
+
+clear VerifiedZ75;
+VerifiedZ76 = clean(replace(Z(7,6), coefList, double(coefList)), 1e-09);
+Z76 = sdisplay(VerifiedZ76);
+
+clear VerifiedZ76;
+VerifiedZ77 = clean(replace(Z(7,7), coefList, double(coefList)), 1e-09);
+Z77 = sdisplay(VerifiedZ77);
+
+clear VerifiedZ77;
+VerifiedZ78 = clean(replace(Z(7,8), coefList, double(coefList)), 1e-09);
+Z78 = sdisplay(VerifiedZ78);
+
+clear VerifiedZ78;
+VerifiedZ81 = clean(replace(Z(8,1), coefList, double(coefList)), 1e-09);
+Z81 = sdisplay(VerifiedZ81);
+
+clear VerifiedZ81;
+VerifiedZ82 = clean(replace(Z(8,2), coefList, double(coefList)), 1e-09);
+Z82 = sdisplay(VerifiedZ82);
+
+clear VerifiedZ82;
+VerifiedZ83 = clean(replace(Z(8,3), coefList, double(coefList)), 1e-09);
+Z83 = sdisplay(VerifiedZ83);
+
+clear VerifiedZ83;
+VerifiedZ84 = clean(replace(Z(8,4), coefList, double(coefList)), 1e-09);
+Z84 = sdisplay(VerifiedZ84);
+
+clear VerifiedZ84;
+VerifiedZ85 = clean(replace(Z(8,5), coefList, double(coefList)), 1e-09);
+Z85 = sdisplay(VerifiedZ85);
+
+clear VerifiedZ85;
+VerifiedZ86 = clean(replace(Z(8,6), coefList, double(coefList)), 1e-09);
+Z86 = sdisplay(VerifiedZ86);
+
+clear VerifiedZ86;
+VerifiedZ87 = clean(replace(Z(8,7), coefList, double(coefList)), 1e-09);
+Z87 = sdisplay(VerifiedZ87);
+
+clear VerifiedZ87;
+VerifiedZ88 = clean(replace(Z(8,8), coefList, double(coefList)), 1e-09);
+Z88 = sdisplay(VerifiedZ88);
+
+clear VerifiedZ88;
+Z = [Z11, Z12, Z13, Z14, Z15, Z16, Z17, Z18;
+     Z21, Z22, Z23, Z24, Z25, Z26, Z27, Z28;
+     Z31, Z32, Z33, Z34, Z35, Z36, Z37, Z38;
+     Z41, Z42, Z43, Z44, Z45, Z46, Z47, Z48;
+     Z51, Z52, Z53, Z54, Z55, Z56, Z57, Z58;
+     Z61, Z62, Z63, Z64, Z65, Z66, Z67, Z68;
+     Z71, Z72, Z73, Z74, Z75, Z76, Z77, Z78;
+     Z81, Z82, Z83, Z84, Z85, Z86, Z87, Z88];
