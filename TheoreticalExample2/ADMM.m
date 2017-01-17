1ff645570dabb0521f1c1d070a974b32aa696812
humberto.shiromoto <humberto.shiromoto@ieee.org>
2017-01-18 10:35:23 +1100

    Consider a bidimensional case.

--- TheoreticalExample2/ADMM.m
+++ TheoreticalExample2/ADMM.m
@@ -11,7 +11,6 @@ function ADMM
 clear all
 clc
 close all
-dbstop if error
 
 %% Initialization =========================================================
 
@@ -24,30 +23,27 @@ SystemInputs = ['u'];
 SystemParameters = ['d'];
 d = 1e-2; %Maximum value to avoid numerical problems
 % d = 0;
-NumberOfAgents = 4
+NumberOfAgents = 2
 NumberOfStates = length(SystemStates);
 
 LaplacianMatrix = LinearLaplacianGenerator(NumberOfAgents);
-% WriterSys(SystemStates,SystemInputs,SystemParameters,Simulation,LaplacianMatrix)
+WriterSys(SystemStates,SystemInputs,SystemParameters,Simulation,LaplacianMatrix)
 % PreProcessedSys
 
 PolymialDegree = 2;
 NumberOfMonomials = nchoosek(2*NumberOfStates + PolymialDegree,PolymialDegree);
 
-MaxNumberOfIterations = 50;
+MaxNumberOfIterations = 10;
 IterationCounter      = 1;
 ErrorTolerance = 1e-2;
 Error = Inf;
 ErrorVector = zeros(MaxNumberOfIterations,1);
 
-Gramian_MI1 = rand(NumberOfMonomials);
-Gramian_MI2 = rand(NumberOfMonomials);
-Gramian_MI3 = rand(NumberOfMonomials);
-Gramian_MI4 = rand(NumberOfMonomials);
+Gramian_MI1 = 10*rand(NumberOfMonomials);
+Gramian_MI2 = 10*rand(NumberOfMonomials);
+Gramian_MI  = blkdiag(Gramian_MI1,Gramian_MI2);
 Lambda1     = rand(NumberOfMonomials);
 Lambda2     = rand(NumberOfMonomials);
-Lambda3     = rand(NumberOfMonomials);
-Lambda4     = rand(NumberOfMonomials);
 
 %% System Definition ======================================================
 
@@ -55,23 +51,18 @@ Lambda4     = rand(NumberOfMonomials);
 
 x1 = sdpvar(1);
 x2 = sdpvar(1);
-x3 = sdpvar(1);
-x4 = sdpvar(1);
 
 y1 = sdpvar(1);
 y2 = sdpvar(1);
-y3 = sdpvar(1);
-y4 = sdpvar(1);
+
 
 % Writing System Inputs: u
 
 u1 = sdpvar(1);
 u2 = sdpvar(1);
-u3 = sdpvar(1);
-u4 = sdpvar(1);
 
 
-% Definition of the Variables q and inputs
+% Definition of the Variables q and inputs 
 
 q1 = [x1; y1];
 input1 = [u1];
@@ -79,30 +70,21 @@ input1 = [u1];
 q2 = [x2; y2];
 input2 = [u2];
 
-q3 = [x3; y3];
-input3 = [u3];
+q = [q1; q2];
 
-q4 = [x4; y4];
-input4 = [u4];
-
-q = [q1; q2; q3; q4];
-
-InputVector = [input1; input2; input3; input4];
+InputVector = [input1; input2];
 
 
-% Writing System Equations
+% Writing System Equations 
 
-x1dot = -x1 - x1^3 + y1^2 -d*(+1*x1^3 -1*x2^3 +0*x3^3 +0*x4^3);
-x2dot = -x2 - x2^3 + y2^2 -d*(-1*x1^3 +2*x2^3 -1*x3^3 +0*x4^3);
-x3dot = -x3 - x3^3 + y3^2 -d*(+0*x1^3 -1*x2^3 +2*x3^3 -1*x4^3);
-x4dot = -x4 - x4^3 + y4^2 -d*(+0*x1^3 +0*x2^3 -1*x3^3 +1*x4^3);
+x1dot = -x1 - x1^3 + y1^2 -d*(+1*x1^3 -1*x2^3);
+x2dot = -x2 - x2^3 + y2^2 -d*(-1*x1^3 +1*x2^3);
 
 y1dot = 0;
 y2dot = 0;
-y3dot = 0;
-y4dot = 0;
 
-% Writing Vector Fields and Jacobian Matrices
+
+% Writing Vector Fields and Jacobian Matrices 
 
 f1 = [x1dot; y1dot];
 A11 = jacobian(f1,q1);
@@ -112,23 +94,12 @@ B1  = jacobian(f1,input1);
 f2 = [x2dot; y2dot];
 A21 = jacobian(f2,q1);
 A22 = jacobian(f2,q2);
-A23 = jacobian(f2,q3);
 B2  = jacobian(f2,input2);
 
-f3 = [x3dot; y3dot];
-A32 = jacobian(f3,q2);
-A33 = jacobian(f3,q3);
-A34 = jacobian(f3,q4);
-B3  = jacobian(f3,input3);
-
-f4 = [x4dot; y4dot];
-A43 = jacobian(f4,q3);
-A44 = jacobian(f4,q4);
-B4  = jacobian(f4,input4);
-
-f = [f1;f2;f3;f4;];
+f = [f1;f2;];
 A = jacobian(f,q);
 B = jacobian(f,InputVector);
+
 tic
 while IterationCounter <= MaxNumberOfIterations && Error >= ErrorTolerance    
     %% W update ===============================================================
@@ -157,30 +128,8 @@ while IterationCounter <= MaxNumberOfIterations && Error >= ErrorTolerance
     Wc2 = [Wc211;Wc212;Wc222];
     Wp2 = [Wp211;Wp212;Wp222];
     
-    % Creation of W3 and DW3
-    [Wp311,Wc311,Wv311] = polynomial(q3,2,0);
-    [Wp312,Wc312,Wv312] = polynomial(q3,2,0);
-    [Wp322,Wc322,Wv322] = polynomial(q3,2,0);
-    % Creation of W3
-    W3 = [Wp311, Wp312;
-        Wp312, Wp322];
-    
-    Wc3 = [Wc311;Wc312;Wc322];
-    Wp3 = [Wp311;Wp312;Wp322];
-    
-    % Creation of W4 and DW4
-    [Wp411,Wc411,Wv411] = polynomial(q4,2,0);
-    [Wp412,Wc412,Wv412] = polynomial(q4,2,0);
-    [Wp422,Wc422,Wv422] = polynomial(q4,2,0);
-    % Creation of W4
-    W4 = [Wp411, Wp412;
-          Wp412, Wp422];
-    
-    Wc4 = [Wc411;Wc412;Wc422];
-    Wp4 = [Wp411;Wp412;Wp422];
-    
-    W = blkdiag(W1,W2,W3,W4);
-    Wc = [Wc1;Wc2;Wc3;Wc4];
+    W = blkdiag(W1,W2);
+    Wc = [Wc1;Wc2];
     
     % Optimization of the W's -------------------------------------------------
     
@@ -245,70 +194,6 @@ while IterationCounter <= MaxNumberOfIterations && Error >= ErrorTolerance
         keyboard;
     end
     
-    % W3
-    
-    v3 = sdpvar(length(W3),1);
-    ListOfMonomials3 = monolist([q3;v3],2);
-    Gramian_W3  = sdpvar(length(ListOfMonomials3));
-    
-    p_W3  = v3'*W3*v3;
-    p_SW3 = ListOfMonomials3'*Gramian_W3*ListOfMonomials3;
-    
-    Options = sdpsettings('solver','mosek','verbose',0);
-    
-    Constraints = [Gramian_W3>=0;coefficients(p_W3-p_SW3,[q3;v3])==0];
-    Objective   = norm(Gramian_W3 - Gramian_MI3 + Lambda3,'fro')^2;
-    Diagnostic3 = optimize(Constraints,Objective,Options);
-    
-    
-    if Diagnostic3.problem ~= 0
-        disp('ERROR:')
-        yalmiperror(Diagnostic3.problem)
-        keyboard;
-    end
-    
-    % Verifying if the Gramian is PSD
-    Gramian_W3 = value(Gramian_W3);
-    [~,EigenValues] = eig(Gramian_W3);
-    EigenValueTolerance = 1e-6;
-    EigenValues = clean(EigenValues,EigenValueTolerance);
-    if min(EigenValues(:)) < 0
-        disp('ERROR: Gramian_W3 is no PSD')
-        keyboard;
-    end
-    
-    % W4
-    
-    v4 = sdpvar(length(W4),1);
-    ListOfMonomials4 = monolist([q4;v4],2);
-    Gramian_W4  = sdpvar(length(ListOfMonomials4));
-    
-    p_W4  = v4'*W4*v4;
-    p_SW4 = ListOfMonomials4'*Gramian_W4*ListOfMonomials4;
-    
-    Options = sdpsettings('solver','mosek','verbose',0);
-    
-    Constraints = [Gramian_W4>=0;coefficients(p_W4-p_SW4,[q4;v4])==0];
-    Objective   = norm(Gramian_W4 - Gramian_MI4 + Lambda4,'fro')^2;
-    Diagnostic4 = optimize(Constraints,Objective,Options);
-    
-    
-    if Diagnostic4.problem ~= 0
-        disp('ERROR:')
-        yalmiperror(Diagnostic4.problem)
-        keyboard;
-    end
-    
-    % Verifying if the Gramian is PSD
-    Gramian_W4 = value(Gramian_W4);
-    [~,EigenValues] = eig(Gramian_W4);
-    EigenValueTolerance = 1e-6;
-    EigenValues = clean(EigenValues,EigenValueTolerance);
-    if min(EigenValues(:)) < 0
-        disp('ERROR: Gramian_W4 is no PSD')
-        keyboard;
-    end
-    
     %% Z Update ===============================================================
     
     % Creation of the metric --------------------------------------------------
@@ -348,69 +233,33 @@ while IterationCounter <= MaxNumberOfIterations && Error >= ErrorTolerance
         0,                DZTemp(3,:)*f2/2];
     DZ2 = DZ2 + transpose(DZ2);
     
-    % Creation of Z3 and DZ3
-    [Zp311,Zc311,~] = polynomial(q3,2,0);
-    [Zp312,Zc312,~] = polynomial(q3,2,0);
-    [Zp322,Zc322,~] = polynomial(q3,2,0);
-    % Creation of Z3
-    Z3 = [Zp311, Zp312;
-        Zp312, Zp322];
-    
-    Zc3 = [Zc311;Zc312;Zc322];
-    Zp3 = [Zp311;Zp312;Zp322];
-    
-    % Creation of DZ3
-    DZTemp = jacobian(Zp3,q3);
-    DZ3 = [DZTemp(1,:)*f3/2, DZTemp(2,:)*f3;
-        0,                DZTemp(3,:)*f3/2];
-    DZ3 = DZ3 + transpose(DZ3);
-    
-    % Creation of Z4 and DZ4
-    [Zp411,Zc411,~] = polynomial(q4,2,0);
-    [Zp412,Zc412,~] = polynomial(q4,2,0);
-    [Zp422,Zc422,~] = polynomial(q4,2,0);
-    % Creation of Z4
-    Z4 = [Zp411, Zp412;
-        Zp412, Zp422];
-    
-    Zc4 = [Zc411;Zc412;Zc422];
-    Zp4 = [Zp411;Zp412;Zp422];
-    
-    % Creation of DZ4
-    DZTemp = jacobian(Zp4,q4);
-    DZ4 = [DZTemp(1,:)*f4/2, DZTemp(2,:)*f4;
-        0,                DZTemp(3,:)*f4/2];
-    DZ4 = DZ4 + transpose(DZ4);
-    
-    Z = blkdiag(Z1,Z2,Z3,Z4);
-    DZ = blkdiag(DZ1,DZ2,DZ3,DZ4);
-    Zc = [Zc1;Zc2;Zc3;Zc4];
+    Z = blkdiag(Z1,Z2);
+    DZ = blkdiag(DZ1,DZ2);
+    Zc = [Zc1;Zc2];
     
     % Definition of the contraction inequality --------------------------------
     
     lambda = 1e-1;
     MI = -DZ + A*Z + Z*transpose(A) + 2*lambda*Z;
     
-    v = [v1;v2;v3;v4];
+    v = [v1;v2];
     
-    ListOfMonomials_MI = [ListOfMonomials1;ListOfMonomials2;ListOfMonomials3;ListOfMonomials4];
-    
-    Gramian_MI1 = sdpvar(length(ListOfMonomials1));
-    Gramian_MI2 = sdpvar(length(ListOfMonomials2));
-    Gramian_MI3 = sdpvar(length(ListOfMonomials3));
-    Gramian_MI4 = sdpvar(length(ListOfMonomials4));
-    Gramian_MI  = blkdiag(Gramian_MI1,Gramian_MI2,Gramian_MI3,Gramian_MI4);
+%     ListOfMonomials_MI = [ListOfMonomials1;ListOfMonomials2];
+    ListOfMonomials_MI = monolist([q1;q2;v1;v2],2);
+
+    Gramian_MI = sdpvar(length(ListOfMonomials_MI));
     
     p_MI = -v'*MI*v;
     p_SMI = ListOfMonomials_MI'*Gramian_MI*ListOfMonomials_MI;
     
+    Options = sdpsettings('solver','mosek','verbose',0);
+    
     Constraints = [Gramian_MI>=0;coefficients(p_MI-p_SMI,[q;v])==0];
-    Objective   = norm(Gramian_MI1 - Gramian_W1 - Lambda1,'fro')^2 ...
-                  + norm(Gramian_MI2 - Gramian_W2 - Lambda2,'fro')^2 ...
-                  + norm(Gramian_MI3 - Gramian_W3 - Lambda3,'fro')^2 ...
-                  + norm(Gramian_MI4 - Gramian_W4 - Lambda4,'fro')^2;
-%     Objective   = norm(Gramian_MI - blkdiag(Gramian_W1,Gramian_W2) ...
-%                   + blkdiag(Lambda1,Lambda2),'fro')^2;
+%     Objective   = norm(Gramian_MI1 - Gramian_W1 - Lambda1,'fro')^2 ...
+%                   + norm(Gramian_MI2 - Gramian_W2 - Lambda2,'fro')^2;
+    Objective   = norm(blkdiag(Gramian_MI1,Gramian_MI2)...
+                   + blkdiag(Gramian_W1,Gramian_W2) -...
+                   + blkdiag(Lambda1,Lambda2),'fro')^2;
     DiagnosticMI = optimize(Constraints,Objective,Options);
     
     % Checking for errors in the optimization ---------------------------------
@@ -420,12 +269,13 @@ while IterationCounter <= MaxNumberOfIterations && Error >= ErrorTolerance
         keyboard;
     end
     
+    Gramian_MI1 = Gramian_MI(1:15,1:15);
+    Gramian_MI2 = Gramian_MI(16:30,16:30);
+    
     % Verifying if the Gramian is PSD -----------------------------------------
     Gramian_MI = value(Gramian_MI);
     Gramian_MI1 = value(Gramian_MI1);
     Gramian_MI2 = value(Gramian_MI2);
-    Gramian_MI3 = value(Gramian_MI3);
-    Gramian_MI4 = value(Gramian_MI4);
     [~,EigenValues] = eig(Gramian_MI);
     EigenValueTolerance = 1e-6;
     EigenValues = clean(EigenValues,EigenValueTolerance);
@@ -438,14 +288,9 @@ while IterationCounter <= MaxNumberOfIterations && Error >= ErrorTolerance
     
     Lambda1 = Gramian_W1 - Gramian_MI1 + Lambda1;
     Lambda2 = Gramian_W2 - Gramian_MI2 + Lambda2;
-    Lambda3 = Gramian_W3 - Gramian_MI3 + Lambda3;
-    Lambda4 = Gramian_W4 - Gramian_MI4 + Lambda4;
     
     ErrorVector(IterationCounter) = norm(Gramian_W1 - Gramian_MI1,'fro')...
-                                    + norm(Gramian_W2 - Gramian_MI2,'fro')...
-                                    + norm(Gramian_W3 - Gramian_MI3,'fro')...
-                                    + norm(Gramian_W4 - Gramian_MI4,'fro');
-%   ErrorVector(IterationCounter) = norm(Lambda1,'fro') + norm(Lambda2,'fro');
+                                    + norm(Gramian_W2 - Gramian_MI2,'fro');
     Error = ErrorVector(IterationCounter);
     IterationCounter = IterationCounter + 1;
     
@@ -468,13 +313,12 @@ grid on
 
 %W
 
-Precision = 1e-9;
+Precision = 1e-6;
 coefList = Wc;
 AnalysisW(SystemStates,LaplacianMatrix,Precision)
 PostProcessedW
 W
 
-Precision = 1e-15;
 coefList = Zc;
 PostProcessedZ
 Z
--- TheoreticalExample2/PostProcessedZ.m
+++ TheoreticalExample2/PostProcessedZ.m
@@ -14,22 +14,6 @@ VerifiedZ14 = clean(replace(Z(1,4), coefList, double(coefList)), Precision);
 Z14 = sdisplay(VerifiedZ14);
 
 clear VerifiedZ14;
-VerifiedZ15 = clean(replace(Z(1,5), coefList, double(coefList)), Precision);
-Z15 = sdisplay(VerifiedZ15);
-
-clear VerifiedZ15;
-VerifiedZ16 = clean(replace(Z(1,6), coefList, double(coefList)), Precision);
-Z16 = sdisplay(VerifiedZ16);
-
-clear VerifiedZ16;
-VerifiedZ17 = clean(replace(Z(1,7), coefList, double(coefList)), Precision);
-Z17 = sdisplay(VerifiedZ17);
-
-clear VerifiedZ17;
-VerifiedZ18 = clean(replace(Z(1,8), coefList, double(coefList)), Precision);
-Z18 = sdisplay(VerifiedZ18);
-
-clear VerifiedZ18;
 VerifiedZ21 = clean(replace(Z(2,1), coefList, double(coefList)), Precision);
 Z21 = sdisplay(VerifiedZ21);
 
@@ -46,22 +30,6 @@ VerifiedZ24 = clean(replace(Z(2,4), coefList, double(coefList)), Precision);
 Z24 = sdisplay(VerifiedZ24);
 
 clear VerifiedZ24;
-VerifiedZ25 = clean(replace(Z(2,5), coefList, double(coefList)), Precision);
-Z25 = sdisplay(VerifiedZ25);
-
-clear VerifiedZ25;
-VerifiedZ26 = clean(replace(Z(2,6), coefList, double(coefList)), Precision);
-Z26 = sdisplay(VerifiedZ26);
-
-clear VerifiedZ26;
-VerifiedZ27 = clean(replace(Z(2,7), coefList, double(coefList)), Precision);
-Z27 = sdisplay(VerifiedZ27);
-
-clear VerifiedZ27;
-VerifiedZ28 = clean(replace(Z(2,8), coefList, double(coefList)), Precision);
-Z28 = sdisplay(VerifiedZ28);
-
-clear VerifiedZ28;
 VerifiedZ31 = clean(replace(Z(3,1), coefList, double(coefList)), Precision);
 Z31 = sdisplay(VerifiedZ31);
 
@@ -78,22 +46,6 @@ VerifiedZ34 = clean(replace(Z(3,4), coefList, double(coefList)), Precision);
 Z34 = sdisplay(VerifiedZ34);
 
 clear VerifiedZ34;
-VerifiedZ35 = clean(replace(Z(3,5), coefList, double(coefList)), Precision);
-Z35 = sdisplay(VerifiedZ35);
-
-clear VerifiedZ35;
-VerifiedZ36 = clean(replace(Z(3,6), coefList, double(coefList)), Precision);
-Z36 = sdisplay(VerifiedZ36);
-
-clear VerifiedZ36;
-VerifiedZ37 = clean(replace(Z(3,7), coefList, double(coefList)), Precision);
-Z37 = sdisplay(VerifiedZ37);
-
-clear VerifiedZ37;
-VerifiedZ38 = clean(replace(Z(3,8), coefList, double(coefList)), Precision);
-Z38 = sdisplay(VerifiedZ38);
-
-clear VerifiedZ38;
 VerifiedZ41 = clean(replace(Z(4,1), coefList, double(coefList)), Precision);
 Z41 = sdisplay(VerifiedZ41);
 
@@ -110,155 +62,10 @@ VerifiedZ44 = clean(replace(Z(4,4), coefList, double(coefList)), Precision);
 Z44 = sdisplay(VerifiedZ44);
 
 clear VerifiedZ44;
-VerifiedZ45 = clean(replace(Z(4,5), coefList, double(coefList)), Precision);
-Z45 = sdisplay(VerifiedZ45);
 
-clear VerifiedZ45;
-VerifiedZ46 = clean(replace(Z(4,6), coefList, double(coefList)), Precision);
-Z46 = sdisplay(VerifiedZ46);
-
-clear VerifiedZ46;
-VerifiedZ47 = clean(replace(Z(4,7), coefList, double(coefList)), Precision);
-Z47 = sdisplay(VerifiedZ47);
-
-clear VerifiedZ47;
-VerifiedZ48 = clean(replace(Z(4,8), coefList, double(coefList)), Precision);
-Z48 = sdisplay(VerifiedZ48);
-
-clear VerifiedZ48;
-VerifiedZ51 = clean(replace(Z(5,1), coefList, double(coefList)), Precision);
-Z51 = sdisplay(VerifiedZ51);
-
-clear VerifiedZ51;
-VerifiedZ52 = clean(replace(Z(5,2), coefList, double(coefList)), Precision);
-Z52 = sdisplay(VerifiedZ52);
-
-clear VerifiedZ52;
-VerifiedZ53 = clean(replace(Z(5,3), coefList, double(coefList)), Precision);
-Z53 = sdisplay(VerifiedZ53);
-
-clear VerifiedZ53;
-VerifiedZ54 = clean(replace(Z(5,4), coefList, double(coefList)), Precision);
-Z54 = sdisplay(VerifiedZ54);
-
-clear VerifiedZ54;
-VerifiedZ55 = clean(replace(Z(5,5), coefList, double(coefList)), Precision);
-Z55 = sdisplay(VerifiedZ55);
-
-clear VerifiedZ55;
-VerifiedZ56 = clean(replace(Z(5,6), coefList, double(coefList)), Precision);
-Z56 = sdisplay(VerifiedZ56);
-
-clear VerifiedZ56;
-VerifiedZ57 = clean(replace(Z(5,7), coefList, double(coefList)), Precision);
-Z57 = sdisplay(VerifiedZ57);
-
-clear VerifiedZ57;
-VerifiedZ58 = clean(replace(Z(5,8), coefList, double(coefList)), Precision);
-Z58 = sdisplay(VerifiedZ58);
-
-clear VerifiedZ58;
-VerifiedZ61 = clean(replace(Z(6,1), coefList, double(coefList)), Precision);
-Z61 = sdisplay(VerifiedZ61);
-
-clear VerifiedZ61;
-VerifiedZ62 = clean(replace(Z(6,2), coefList, double(coefList)), Precision);
-Z62 = sdisplay(VerifiedZ62);
-
-clear VerifiedZ62;
-VerifiedZ63 = clean(replace(Z(6,3), coefList, double(coefList)), Precision);
-Z63 = sdisplay(VerifiedZ63);
-
-clear VerifiedZ63;
-VerifiedZ64 = clean(replace(Z(6,4), coefList, double(coefList)), Precision);
-Z64 = sdisplay(VerifiedZ64);
-
-clear VerifiedZ64;
-VerifiedZ65 = clean(replace(Z(6,5), coefList, double(coefList)), Precision);
-Z65 = sdisplay(VerifiedZ65);
-
-clear VerifiedZ65;
-VerifiedZ66 = clean(replace(Z(6,6), coefList, double(coefList)), Precision);
-Z66 = sdisplay(VerifiedZ66);
-
-clear VerifiedZ66;
-VerifiedZ67 = clean(replace(Z(6,7), coefList, double(coefList)), Precision);
-Z67 = sdisplay(VerifiedZ67);
-
-clear VerifiedZ67;
-VerifiedZ68 = clean(replace(Z(6,8), coefList, double(coefList)), Precision);
-Z68 = sdisplay(VerifiedZ68);
-
-clear VerifiedZ68;
-VerifiedZ71 = clean(replace(Z(7,1), coefList, double(coefList)), Precision);
-Z71 = sdisplay(VerifiedZ71);
-
-clear VerifiedZ71;
-VerifiedZ72 = clean(replace(Z(7,2), coefList, double(coefList)), Precision);
-Z72 = sdisplay(VerifiedZ72);
-
-clear VerifiedZ72;
-VerifiedZ73 = clean(replace(Z(7,3), coefList, double(coefList)), Precision);
-Z73 = sdisplay(VerifiedZ73);
-
-clear VerifiedZ73;
-VerifiedZ74 = clean(replace(Z(7,4), coefList, double(coefList)), Precision);
-Z74 = sdisplay(VerifiedZ74);
-
-clear VerifiedZ74;
-VerifiedZ75 = clean(replace(Z(7,5), coefList, double(coefList)), Precision);
-Z75 = sdisplay(VerifiedZ75);
-
-clear VerifiedZ75;
-VerifiedZ76 = clean(replace(Z(7,6), coefList, double(coefList)), Precision);
-Z76 = sdisplay(VerifiedZ76);
-
-clear VerifiedZ76;
-VerifiedZ77 = clean(replace(Z(7,7), coefList, double(coefList)), Precision);
-Z77 = sdisplay(VerifiedZ77);
-
-clear VerifiedZ77;
-VerifiedZ78 = clean(replace(Z(7,8), coefList, double(coefList)), Precision);
-Z78 = sdisplay(VerifiedZ78);
-
-clear VerifiedZ78;
-VerifiedZ81 = clean(replace(Z(8,1), coefList, double(coefList)), Precision);
-Z81 = sdisplay(VerifiedZ81);
-
-clear VerifiedZ81;
-VerifiedZ82 = clean(replace(Z(8,2), coefList, double(coefList)), Precision);
-Z82 = sdisplay(VerifiedZ82);
-
-clear VerifiedZ82;
-VerifiedZ83 = clean(replace(Z(8,3), coefList, double(coefList)), Precision);
-Z83 = sdisplay(VerifiedZ83);
-
-clear VerifiedZ83;
-VerifiedZ84 = clean(replace(Z(8,4), coefList, double(coefList)), Precision);
-Z84 = sdisplay(VerifiedZ84);
-
-clear VerifiedZ84;
-VerifiedZ85 = clean(replace(Z(8,5), coefList, double(coefList)), Precision);
-Z85 = sdisplay(VerifiedZ85);
-
-clear VerifiedZ85;
-VerifiedZ86 = clean(replace(Z(8,6), coefList, double(coefList)), Precision);
-Z86 = sdisplay(VerifiedZ86);
-
-clear VerifiedZ86;
-VerifiedZ87 = clean(replace(Z(8,7), coefList, double(coefList)), Precision);
-Z87 = sdisplay(VerifiedZ87);
-
-clear VerifiedZ87;
-VerifiedZ88 = clean(replace(Z(8,8), coefList, double(coefList)), Precision);
-Z88 = sdisplay(VerifiedZ88);
 
 clear VerifiedZ88;
-Z = [Z11, Z12, Z13, Z14, Z15, Z16, Z17, Z18;
-     Z21, Z22, Z23, Z24, Z25, Z26, Z27, Z28;
-     Z31, Z32, Z33, Z34, Z35, Z36, Z37, Z38;
-     Z41, Z42, Z43, Z44, Z45, Z46, Z47, Z48;
-     Z51, Z52, Z53, Z54, Z55, Z56, Z57, Z58;
-     Z61, Z62, Z63, Z64, Z65, Z66, Z67, Z68;
-     Z71, Z72, Z73, Z74, Z75, Z76, Z77, Z78;
-     Z81, Z82, Z83, Z84, Z85, Z86, Z87, Z88];
+Z = [Z11, Z12, Z13, Z14;
+     Z21, Z22, Z23, Z24;
+     Z31, Z32, Z33, Z34;
+     Z41, Z42, Z43, Z44];
